1fa8530f87c696f8dd4e4e6272416b8ef44b772d
Jens Vagelpohl <jens@dataflake.org>
2005-07-17 10:04:16 +0000

    Migration reorg
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/tags/CMF-1_4_2-release@37167 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ CMFDefault/RegistrationTool.py
@@ -0,0 +1,203 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+""" CMFDefault portal_registration tool.
+
+$Id$
+"""
+
+from Globals import InitializeClass, DTMLFile
+from AccessControl import ClassSecurityInfo
+
+from Products.CMFCore.utils import _checkPermission
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.ActionInformation import ActionInformation
+from Products.CMFCore.Expression import Expression
+from Products.CMFCore.ActionProviderBase import ActionProviderBase
+from Products.CMFCore.RegistrationTool import RegistrationTool as BaseTool
+from Products.CMFCore.CMFCorePermissions import AddPortalMember
+from Products.CMFCore.CMFCorePermissions import ManagePortal
+
+from utils import _dtmldir
+
+class RegistrationTool(BaseTool):
+    """ Manage through-the-web signup policies.
+    """
+
+    __implements__ = BaseTool.__implements__
+
+    meta_type = 'Default Registration Tool'
+    _actions = ( ActionInformation( id='join'
+                                  , title='Join'
+                                  , description='Click here to Join'
+                                  , action=Expression(
+                                     text='string: ${portal_url}/join_form')
+                                  , permissions=(AddPortalMember,)
+                                  , category='user'
+                                  , condition=Expression(text='not: member')
+                                  , visible=1
+                                  )
+               ,
+               )
+
+    security = ClassSecurityInfo()
+
+    #
+    #   ZMI methods
+    #
+    security.declareProtected( ManagePortal, 'manage_overview' )
+
+    manage_options = ( ActionProviderBase.manage_options
+                     + ( { 'label' : 'Overview'
+                         , 'action' : 'manage_overview'
+                         }
+                       , 
+                       )
+                     )
+    manage_overview = DTMLFile( 'explainRegistrationTool', _dtmldir )
+
+    #
+    #   'portal_registration' interface
+    #
+    security.declarePublic( 'testPasswordValidity' )
+    def testPasswordValidity(self, password, confirm=None):
+
+        """ Verify that the password satisfies the portal's requirements.
+        
+        o If the password is valid, return None.
+        o If not, return a string explaining why.
+        """
+        if len(password) < 5 and not _checkPermission('Manage portal', self):
+            return 'Your password must contain at least 5 characters.'
+
+        if confirm is not None and confirm != password:
+            return ( 'Your password and confirmation did not match. ' 
+                   + 'Please try again.' )
+
+        return None
+
+    security.declarePublic( 'testPropertiesValidity' )
+    def testPropertiesValidity(self, props, member=None):
+
+        """ Verify that the properties supplied satisfy portal's requirements.
+
+        o If the properties are valid, return None.
+        o If not, return a string explaining why.
+        """
+        if member is None: # New member.
+
+            username = props.get('username', '')
+            if not username:
+                return 'You must enter a valid name.'
+
+            if not self.isMemberIdAllowed(username):
+                return ('The login name you selected is already '
+                        'in use or is not valid. Please choose another.')
+
+            if not props.get('email'):
+                return 'You must enter a valid email address.'
+
+        else: # Existing member.
+            # Not allowed to clear an existing non-empty email.
+            if (member.getProperty('email') and
+                not props.get('email', 'NoPropIsOk')):
+                return 'You must enter a valid email address.'
+
+        return None
+
+    security.declarePublic( 'mailPassword' )
+    def mailPassword(self, forgotten_userid, REQUEST):
+
+        """ Email a forgotten password to a member.
+        
+        o Raise an exception if user ID is not found.
+        """
+        membership = getToolByName(self, 'portal_membership')
+        member = membership.getMemberById(forgotten_userid)
+
+        if member is None:
+            raise 'NotFound', 'The username you entered could not be found.'
+    
+        # assert that we can actually get an email address, otherwise
+        # the template will be made with a blank To:, this is bad
+        if not member.getProperty('email'):
+            raise 'ValueError', 'That user does not have an email address.'
+        
+        # Rather than have the template try to use the mailhost, we will
+        # render the message ourselves and send it from here (where we
+        # don't need to worry about 'UseMailHost' permissions).
+        mail_text = self.mail_password_template( self
+                                               , REQUEST
+                                               , member=member
+                                               , password=member.getPassword()
+                                               )
+    
+        host = self.MailHost
+        host.send( mail_text )
+
+        return self.mail_password_response( self, REQUEST )
+
+    security.declarePublic( 'registeredNotify' )
+    def registeredNotify( self, new_member_id ):
+
+        """ Handle mailing the registration / welcome message.
+        """
+        membership = getToolByName( self, 'portal_membership' )
+        member = membership.getMemberById( new_member_id )
+
+        if member is None:
+            raise 'NotFound', 'The username you entered could not be found.'
+
+        password = member.getPassword()
+        email = member.getProperty( 'email' )
+
+        if email is None:
+            raise ValueError( 'Member %s has no e-mail address!'
+                            % new_member_id )
+    
+        # Rather than have the template try to use the mailhost, we will
+        # render the message ourselves and send it from here (where we
+        # don't need to worry about 'UseMailHost' permissions).
+        mail_text = self.registered_notify_template( self
+                                                   , self.REQUEST
+                                                   , member=member
+                                                   , password=password
+                                                   , email=email
+                                                   )
+    
+        host = self.MailHost
+        host.send( mail_text )
+
+        return self.mail_password_response( self, self.REQUEST )
+
+    security.declareProtected(ManagePortal, 'editMember')
+    def editMember( self
+                  , member_id
+                  , properties=None
+                  , password=None
+                  , roles=None
+                  , domains=None
+                  ):
+        """ Edit a user's properties and security settings
+
+        o Checks should be done before this method is called using
+          testPropertiesValidity and testPasswordValidity
+        """
+        
+        mtool = getToolByName(self, 'portal_membership')
+        member = mtool.getMemberById(member_id)
+        member.setMemberProperties(properties)
+        member.setSecurityProfile(password,roles,domains)
+
+        return member
+
+InitializeClass(RegistrationTool)
---
+++ CMFDefault/SkinnedFolder.py
@@ -0,0 +1,106 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+""" Allow the "view" of a folder to be skinned by type.
+
+$Id$
+"""
+
+from Products.CMFCore.PortalFolder import PortalFolder
+from Products.CMFCore.CMFCorePermissions import View
+from Products.CMFCore.CMFCorePermissions import ManageProperties
+from Products.CMFCore.CMFCorePermissions import ListFolderContents
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
+from Products.CMFCore.utils import _getViewFor
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+from Acquisition import aq_base
+
+factory_type_information = (
+  { 'id'             : 'Skinned Folder'
+  , 'meta_type'      : 'Skinned Folder'
+  , 'description'    : """\
+Skinned folders can define custom 'view' actions.
+"""
+  , 'icon'           : 'folder_icon.gif'
+  , 'product'        : 'CMFDefault'
+  , 'factory'        : 'addSkinnedFolder'
+  , 'filter_content_types' : 0
+  , 'immediate_view' : 'folder_edit_form'
+  , 'actions'        : ( { 'id'            : 'view' 
+                         , 'name'          : 'View'
+                         , 'action': 'string:${object_url}/folder_view'
+                         , 'permissions'   : (View,)
+                         , 'category'      : 'folder'
+                         }
+                       , { 'id'            : 'edit'
+                         , 'name'          : 'Edit'
+                         , 'action': 'string:${object_url}/folder_edit_form'
+                         , 'permissions'   : (ManageProperties,)
+                         , 'category'      : 'folder'
+                         }
+                       , { 'id'            : 'foldercontents'
+                         , 'name'          : 'Folder contents'
+                         , 'action': 'string:${object_url}/folder_contents'
+                         , 'permissions'   : (ListFolderContents,)
+                         , 'category'      : 'folder'
+                         }
+                       )
+  }
+,
+)
+
+
+class SkinnedFolder(CMFCatalogAware, PortalFolder):
+    """
+    """
+    meta_type = 'Skinned Folder'
+
+    security = ClassSecurityInfo()
+
+    manage_options = PortalFolder.manage_options
+
+    def __call__(self):
+        '''
+        Invokes the default view.
+        '''
+        view = _getViewFor(self)
+        if getattr(aq_base(view), 'isDocTemp', 0):
+            return apply(view, (self, self.REQUEST))
+        else:
+            return view()
+
+    security.declareProtected(View, 'view')
+    view = __call__
+
+    index_html = None  # This special value informs ZPublisher to use __call__
+
+    security.declareProtected(View, 'Creator')
+    def Creator( self ):
+        """
+            Return the ID of our owner.
+        """
+        return self.getOwner( info=1 )[1]
+
+    # We derive from CMFCatalogAware first, so we are cataloged too.
+
+InitializeClass( SkinnedFolder )
+
+def addSkinnedFolder( self, id, title='', description='', REQUEST=None ):
+    """
+    """
+    sf = SkinnedFolder( id, title )
+    sf.description = description
+    self._setObject( id, sf )
+    sf = self._getOb( id )
+    if REQUEST is not None:
+        REQUEST['RESPONSE'].redirect( sf.absolute_url() + '/manage_main' )

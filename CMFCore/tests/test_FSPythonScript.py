b5b4d3410c4adf486c3ed5f7a5560444aa66a221
Jens Vagelpohl <jens@dataflake.org>
2005-07-17 10:04:16 +0000

    Migration reorg
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/tags/CMF-1_3-release@37167 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ CMFCore/tests/test_FSPythonScript.py
@@ -0,0 +1,26 @@
+import Zope
+from unittest import TestCase, TestSuite, makeSuite, main
+from Products.CMFCore.FSPythonScript import FSPythonScript
+from test_DirectoryView import skin_path_name
+from os.path import join
+
+script_path = join(skin_path_name,'test1.py')
+
+class FSPythonScriptTests( TestCase ):
+
+    def test_GetSize( self ):
+        """ Test get_size returns correct value """
+        script = FSPythonScript('test1', script_path)
+        self.assertEqual(len(script.read()),script.get_size())
+
+def test_suite():
+    return TestSuite((
+        makeSuite(FSPythonScriptTests),
+        ))
+
+if __name__ == '__main__':
+    main(defaultTest='test_suite')
+
+
+
+

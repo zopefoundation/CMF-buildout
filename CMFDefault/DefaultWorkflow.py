fe35c6cc5d1da4afc70989a75882ff45d2422343
Julien Anguenot <julien@anguenot.org>
2005-07-18 14:20:02 +0000

    Fix DefaultWorkflowDefinition bug on isActionSupported() for the keywargs support to reflect DCWorkflowDefinition changes.  Add test case for this definition as well.
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/branches/CMF-1_5-branch@37237 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFDefault/DefaultWorkflow.py
+++ CMFDefault/DefaultWorkflow.py
@@ -168,7 +168,7 @@ class DefaultWorkflowDefinition (SimpleItemWithProperties):
         return actions
 
     security.declarePrivate('isActionSupported')
-    def isActionSupported(self, ob, action):
+    def isActionSupported(self, ob, action, **kw):
         '''
         Returns a true value if the given action name is supported.
         '''

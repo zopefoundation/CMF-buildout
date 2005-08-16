de1d2b2052f9dbd41dc440bbe7f0340f8c22a763
Jens Vagelpohl <jens@dataflake.org>
2005-08-16 20:54:10 +0000

    - CMFCore.FSImage and FSFile: Unlike the current behavior of Zope itself,   FSImage and FSFile would set a content-length response header for 304   (not modified) responses, which should not be done according to   RFC 2616. It won't do so anymore, but Zope itself will still force a   content-length header in ZServer.HTTPResponse. This misbehavior   has been filed as a Zope issue (http://www.zope.org/Collectors/Zope/1866).   (http://www.zope.org/Collectors/CMF/372)
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/branches/1.5@37972 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFCore/tests/test_FSFile.py
+++ CMFCore/tests/test_FSFile.py
@@ -120,9 +120,9 @@ class FSFileTests( RequestTest, FSDVTest):
         data = file.index_html( self.REQUEST, self.RESPONSE )
 
         self.assertEqual( data, '' )
-        # test that we properly hack around apache bug noted in code
+        # test that we don't supply a content-length
         self.assertEqual( self.RESPONSE.getHeader('Content-Length'.lower()),
-                                                  str(len(ref)) )
+                                               None )
         self.assertEqual( self.RESPONSE.getStatus(), 304 )
 
     def test_index_html_without_304( self ):

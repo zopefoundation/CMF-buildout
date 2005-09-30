bfcf01303eb2ea6805b2d0ec582bd3a8bd675cbc
Tres Seaver <tseaver@palladion.com>
2005-09-30 22:53:49 +0000

    - Add content interfaces for link objects.
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/trunk@38708 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFDefault/interfaces/_content.py
+++ CMFDefault/interfaces/_content.py
@@ -16,15 +16,14 @@ $Id$
 """
 
 from zope.interface import Interface
+from zope.interface import Attribute
 
 
 class IDocument(Interface):
-
     """ Textual content, in one of several formats.
 
     o Allowed formats include: structured text, HTML, plain text.
     """
-
     def CookedBody():
         """ Get the "cooked" (ready for presentation) form of the text.
         """
@@ -35,13 +34,32 @@ class IDocument(Interface):
 
 
 class IMutableDocument(IDocument):
-
     """ Updatable form of IDocument.
     """
-
     def edit(text_format, text, file='', safety_belt=''):
         """ Update the document.
 
         o 'safety_belt', if passed, must match the value issued when the edit
         began.
+        """
+
+
+class ILink(Interface):
+    """ URL as content.
+    """
+    def getRemoteUrl():
+        """ Return the URL to which the link points, as a string.
+        """
+
+
+class IMutableLink(ILink):
+
+    """ Updatable form of ILink.
+    """
+    def edit(remote_url):
+        """ Update the link.
+
+        o 'remote_url' should be a URL in an RFC-compatible form.
+
+        o If 'remote_url' is unparseable by urllib, raise ValueError.
         """

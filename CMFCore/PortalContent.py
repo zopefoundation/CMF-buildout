9b71838bada6f15720a7aaad9469e48f9fdc8a32
Tres Seaver <tseaver@palladion.com>
2005-11-29 01:58:24 +0000

    Repair broken security assertions.
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/trunk@40395 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFCore/PortalContent.py
+++ CMFCore/PortalContent.py
@@ -69,7 +69,6 @@ class PortalContent(DynamicType, CMFCatalogAware, SimpleItem):
     # The security for FTP methods aren't set up by default in our
     # superclasses...  :(
     security.declareProtected(FTPAccess, 'manage_FTPstat')
-    security.declareProtected(FTPAccess, 'manage_FTPget')
     security.declareProtected(FTPAccess, 'manage_FTPlist')
 
     def failIfLocked(self):

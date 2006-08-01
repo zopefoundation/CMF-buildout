03aedc3e75fbbc7acfb37c7c2be78d8a7460143c
Florent Guillaume <fg@nuxeo.com>
2006-08-01 13:30:37 +0000

    Typo that is 5 year old... ;)
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/branches/1.6@69325 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFDefault/skins/control/folder_paste.py
+++ CMFDefault/skins/control/folder_paste.py
@@ -2,7 +2,7 @@
 ##title=Paste objects to a folder from the clipboard
 ##parameters=
 REQUEST=context.REQUEST
-if context.cb_dataValid:
+if context.cb_dataValid():
   context.manage_pasteObjects(REQUEST['__cp'])
   return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Item(s)+Pasted.')
 else:

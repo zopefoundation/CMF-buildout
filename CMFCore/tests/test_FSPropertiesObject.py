a05134ef26b76aee812aa10532cec8d042754275
Tres Seaver <tseaver@palladion.com>
2006-09-08 17:50:10 +0000

    Forward port test cleanup / expansion from 2.0 branch.
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/trunk@70074 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFCore/tests/test_FSPropertiesObject.py
+++ CMFCore/tests/test_FSPropertiesObject.py
@@ -1,57 +1,88 @@
 import unittest
-import Testing
 
-from os.path import join
-
-from OFS.Folder import Folder
-
-from Products.CMFCore.FSPropertiesObject import FSPropertiesObject
 from Products.CMFCore.tests.base.testcase import FSDVTest
 from Products.CMFCore.tests.base.testcase import SecurityTest
 
-
-class FSPOMaker(FSDVTest):
-
-    def _makeOne( self, id, filename ):
-        path = join(self.skin_path_name, filename)
-        return FSPropertiesObject( id, path ) 
-
-
-class FSPropertiesObjectCustomizationTests(SecurityTest, FSPOMaker):
+class FSPOTests(SecurityTest, FSDVTest):
 
     def setUp( self ):
-        FSPOMaker.setUp(self)
+        FSDVTest.setUp(self)
         SecurityTest.setUp( self )
 
+    def tearDown( self ):
+        SecurityTest.tearDown( self )
+        FSDVTest.tearDown(self)
+
+    def _getTargetClass( self ):
+        from Products.CMFCore.FSPropertiesObject import FSPropertiesObject
+        return FSPropertiesObject
+
+    def _makeOne( self, id, filename ):
+        from os.path import join
+        path = join(self.skin_path_name, filename)
+        return self._getTargetClass()( id, path ) 
+
+    def _makeContext( self, po_id, po_filename ):
+        from OFS.Folder import Folder
+
         self.root._setObject( 'portal_skins', Folder( 'portal_skins' ) )
-        self.skins = self.root.portal_skins
+        skins = self.root.portal_skins
 
-        self.skins._setObject( 'custom', Folder( 'custom' ) )
-        self.custom = self.skins.custom
+        skins._setObject( 'custom', Folder( 'custom' ) )
+        custom = skins.custom
 
-        self.skins._setObject( 'fsdir', Folder( 'fsdir' ) )
-        self.fsdir = self.skins.fsdir
+        skins._setObject( 'fsdir', Folder( 'fsdir' ) )
+        fsdir = skins.fsdir
 
-        self.fsdir._setObject( 'test_props'
-                             , self._makeOne( 'test_props', 'test_props.props' ) )
+        fsdir._setObject( 'test_props', self._makeOne( po_id, po_filename ) )
+        fspo = fsdir.test_props
 
-        self.fsPO = self.fsdir.test_props
+        return custom, fsdir, fspo
 
-    def tearDown(self):
-        SecurityTest.tearDown(self)
-        FSPOMaker.tearDown(self)
+    def test__readFile( self ):
+        from DateTime.DateTime import DateTime
 
-    def test_customize( self ):
+        custom, fsdir, fspo = self._makeContext( 'test_props'
+                                               , 'test_props.props')
 
-        self.fsPO.manage_doCustomize( folder_path='custom' )
+        self.assertEqual( fspo.getProperty( 'title' ), 'Test properties' )
+        self.assertEqual( fspo.getProperty( 'value1' ), 'one' )
+        self.assertEqual( fspo.getProperty( 'value2' ), 'two' )
+        self.assertEqual( fspo.getProperty( 'an_int' ), 42 )
+        self.assertEqual( fspo.getProperty( 'a_float' ), 3.1415926 )
+        self.assertEqual( fspo.getProperty( 'a_boolean' ), False )
+        self.assertEqual( fspo.getProperty( 'a_long' ), 40000000000 )
+        self.assertEqual( fspo.getProperty( 'a_date' )
+                        , DateTime( '01/01/2001' ) )
+        self.assertEqual( fspo.getProperty( 'a_tokens' )
+                        , [ 'peter', 'paul', 'mary' ] )
 
-        self.assertEqual( len( self.custom.objectIds() ), 1 )
-        self.failUnless( 'test_props' in self.custom.objectIds() )  
+    def test__createZODBClone( self ):
+
+        from OFS.Folder import Folder
+
+        custom, fsdir, fspo = self._makeContext( 'test_props'
+                                               , 'test_props.props')
+
+        target = fspo._createZODBClone()
+        self.failUnless( isinstance( target, Folder ) )
+        for prop_id in fspo.propertyIds():
+            self.assertEqual( target.getProperty( prop_id )
+                            , fspo.getProperty( prop_id ) )
+
+    def test_manage_doCustomize( self ):
+        custom, fsdir, fspo = self._makeContext( 'test_props'
+                                               , 'test_props.props')
+
+        fspo.manage_doCustomize( folder_path='custom' )
+
+        self.assertEqual( len( custom.objectIds() ), 1 )
+        self.failUnless( 'test_props' in custom.objectIds() )  
 
 
 def test_suite():
     return unittest.TestSuite((
-        unittest.makeSuite(FSPropertiesObjectCustomizationTests),
+        unittest.makeSuite( FSPOTests ),
         ))
 
 if __name__ == '__main__':

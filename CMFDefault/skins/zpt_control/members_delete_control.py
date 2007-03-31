08a3f57c646d28966e562e1cda75dbddff19f959
Alec Mitchell <alecpm@gmail.com>
2007-03-31 17:26:09 +0000

    Patch from bloodbare to ensure that the postonly protection is used from scripts.
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/branches/1.6@73956 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFDefault/skins/zpt_control/members_delete_control.py
+++ CMFDefault/skins/zpt_control/members_delete_control.py
@@ -5,7 +5,7 @@ from Products.CMFCore.utils import getToolByName
 
 mtool = getToolByName(script, 'portal_membership')
 
-mtool.deleteMembers(ids)
+mtool.deleteMembers(ids,REQUEST=context.REQUEST)
 
 return context.setStatus( True, 'Selected member%s deleted.' %
                                 ( len(ids) != 1 and 's' or '' ) )

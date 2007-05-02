1a8102e7830dad20b30c39d1ade6e12aeb006fbb
Stefan H. Holek <stefan@epy.co.at>
2007-05-02 21:06:43 +0000

    Reverted r74060 and r74136 as they broke more than they fixed. It appears that testSetUp and testTearDown are run before/after every *test*.
    See http://mail.zope.org/pipermail/cmf-tests/2007-May/date.html
    
    
    git-svn-id: svn+ssh://svn.zope.org/repos/main/CMF/trunk@75029 62d5b8a3-27da-0310-9561-8e5933582275

--- CMFCore/testing.py
+++ CMFCore/testing.py
@@ -84,12 +84,10 @@ class EventZCMLLayer:
         zcml.load_config('event.zcml', Products.Five)
         zcml.load_config('event.zcml', Products.CMFCore)
         setHooks()
-    setUp = testSetUp  # forward-compatibility for Zope 2.11+ testrunner
 
     @classmethod
     def testTearDown(cls):
         cleanUp()
-    tearDown = testTearDown  # forward-compatibility for Zope 2.11+ testrunner
 
 
 class TraversingZCMLLayer:
@@ -101,12 +99,10 @@ class TraversingZCMLLayer:
         zcml.load_config('meta.zcml', Products.Five)
         zcml.load_config('traversing.zcml', Products.Five)
         setHooks()
-    setUp = testSetUp  # forward-compatibility for Zope 2.11+ testrunner
 
     @classmethod
     def testTearDown(cls):
         cleanUp()
-    tearDown = testTearDown  # forward-compatibility for Zope 2.11+ testrunner
 
 
 class TraversingEventZCMLLayer:
@@ -120,12 +116,10 @@ class TraversingEventZCMLLayer:
         zcml.load_config('event.zcml', Products.Five)
         zcml.load_config('event.zcml', Products.CMFCore)
         setHooks()
-    setUp = testSetUp  # forward-compatibility for Zope 2.11+ testrunner
 
     @classmethod
     def testTearDown(cls):
         cleanUp()
-    tearDown = testTearDown  # forward-compatibility for Zope 2.11+ testrunner
 
 
 class FunctionalZCMLLayer:
@@ -199,12 +193,10 @@ class ExportImportZCMLLayer:
         zcml.load_config('configure.zcml', Products.CMFCore.exportimport)
         zcml.load_string(_DUMMY_ZCML)
         setHooks()
-    setUp = testSetUp  # forward-compatibility for Zope 2.11+ testrunner
 
     @classmethod
     def testTearDown(cls):
         cleanUp()
-    tearDown = testTearDown  # forward-compatibility for Zope 2.11+ testrunner
 
 
 def run(test_suite):
